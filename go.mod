2e69f8f227c9222814963e73007d9703909dd226
mienord <maxine2003@hotmail.com>
2023-04-20 12:36:03 +0000

    nytt

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mienord/is105sem03/tree/main/mycrypt
+
+go 1.18
+
+require github.com/mienord/is105sem03 v0.0.0-20230419201409-036abcfac5ef
--- tcp_server.go
+++ tcp_server.go
@@ -1,56 +1,65 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net"
-	"sync"
+        "io"
+        "log"
+        "net"
+        "sync"
+        "github.com/mienord/is105sem03/mycrypt"
 )
 
 func main() {
+        var wg sync.WaitGroup
 
-	var wg sync.WaitGroup
-
-	server, err := net.Listen("tcp", "172.17.0.3:8010")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("bundet til %s", server.Addr().String())
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			log.Println("før server.Accept() kallet")
-			conn, err := server.Accept()
-			if err != nil {
-				return
-			}
-			go func(c net.Conn) {
-				defer c.Close()
-				for {
-					buf := make([]byte, 1024)
-					n, err := c.Read(buf)
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-					switch msg := string(buf[:n]); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
+        server, err := net.Listen("tcp", "172.17.0.3:8080")
+        if err != nil {
+                log.Fatal(err)
+        }
+        log.Printf("bundet til %s", server.Addr().String())
+        wg.Add(1)
+        go func() {
+                defer wg.Done()
+                for {
+                        log.Println("f  r server.Accept() kallet")
+                        conn, err := server.Accept()
+                        if err != nil {
+                                return
+                        }
+                        go func(c net.Conn) {
+                                defer c.Close()
+                                for {
+                                        buf := make([]byte, 1024)
+                                        n, err := c.Read(buf)
+                                        if err != nil {
+                                                if err != io.EOF {
+                                                        log.Println(err)
+                                                }
+                                                return
+                                        }
+                                        log.Println("Krypter melding: ", string(buf[:n]))
+                                        cryptedMsg := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+                                        log.Println("Kryptert melding: ", string(cryptedMsg))
+					
+					switch msg := string(cryptedMsg); msg {
+					case "ping":
+    					resp := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+    						_, err = c.Write([]byte(resp))
 					default:
-						_, err = c.Write(buf[:n])
+    					resp := mycrypt.Krypter([]rune(msg), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+    						_, err = c.Write([]byte(string(resp)))
 					}
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-				}
-			}(conn)
-		}
-	}()
-	wg.Wait()
+                                        if err != nil {
+                                                if err != io.EOF {
+                                                        log.Println(err)                                                }
+                                                return
+                                        }
+                                }
+                        }(conn)
+                }
+        }()
+        wg.Wait()
 }
+
+
+
+üüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüüü�éééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééé
