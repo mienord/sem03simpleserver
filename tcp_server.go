5cb187487bcd8310e49a064267c02296cf6bb6ae
mienord <111752047+mienord@users.noreply.github.com>
2023-04-20 17:05:33 +0200

    Update tcp_server.go

--- tcp_server.go
+++ tcp_server.go
@@ -20,7 +20,7 @@ func main() {
         go func() {
                 defer wg.Done()
                 for {
-                        log.Println("f  r server.Accept() kallet")
+                        log.Println("før server.Accept() kallet")
                         conn, err := server.Accept()
                         if err != nil {
                                 return
@@ -37,7 +37,7 @@ func main() {
                                                 return
                                         }
                                         log.Println("Krypter melding: ", string(buf[:n]))
-                                        cryptedMsg := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03) 4)
+                                        cryptedMsg := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, 4)
                                         log.Println("Kryptert melding: ", string(cryptedMsg))
 					
 					switch msg := string(cryptedMsg); msg {
